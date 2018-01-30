5268caa43ce603f786f1e20fcbd8c0375d558a7f
Eivind Hjertnes <hjertnes@gmail.com>
2018-01-30 08:41:58 +0100

    Update mb.el
    Added confirmation before posting

--- mb.el
+++ mb.el
@@ -1,9 +1,11 @@
 (require 'request)
 (defun post2mb ()
   (interactive)
-  (request
-   "https://micro.blog/micropub"
-   :type "POST"
-   :params `(("h"."entry")("content" . ,(buffer-substring-no-properties (point-min) (point-max))))
-   :headers `(("Authorization" . ,(format "Bearer %s" microblog-auth)))
-   :success (cl-function (lambda (&key data &allow-other-keys) (message "Success")))))
+  (if (yes-or-no-p "Are you sure you want to post this?" )
+    (request
+      "https://micro.blog/micropub"
+      :type "POST"
+      :params `(("h"."entry")("content" . ,(buffer-substring-no-properties (point-min) (point-max))))
+      :headers `(("Authorization" . ,(format "Bearer %s" microblog-auth)))
+      :success (cl-function (lambda (&key data &allow-other-keys) (message "Success"))))))
+
