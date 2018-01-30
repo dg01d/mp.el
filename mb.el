052a56ca761991e80c82bc00433c38bcb166f198
Eivind Hjertnes <me@hjertnes.me>
2018-01-30 18:27:47 +0100

    Renamed micro-blog-auth to mb-auth to make it more consistent acorss both scripts

--- mb.el
+++ mb.el
@@ -11,6 +11,6 @@
       (format "%s/micropub" (get-url))
       :type "POST"
       :params `(("h"."entry")("content" . ,(buffer-substring-no-properties (point-min) (point-max))))
-      :headers `(("Authorization" . ,(format "Bearer %s" microblog-auth)))
+      :headers `(("Authorization" . ,(format "Bearer %s" mb-auth)))
       :success (cl-function (lambda (&key data &allow-other-keys) (message "Success"))))))
 
